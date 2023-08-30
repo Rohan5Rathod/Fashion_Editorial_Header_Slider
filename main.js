ec74386073a7ece1025947ea4fa2eb436a3c1627
Rohan Rathod <rohanrathod2015@gmail.com>
2023-08-30 18:36:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "fade",
+  speed: 600,
+  parallax: true,
+  loop: true,
+  autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  navigation: {
+    nextEl: ".ri-arrow-left-line",
+    prevEl: ".ri-arrow-right-line",
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  effect: "fade",
+  speed: 600,
+  parallax: true,
+  loop: true,
+  autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  navigation: {
+    nextEl: ".ri-arrow-left-line",
+    prevEl: ".ri-arrow-right-line",
+  },
+});
